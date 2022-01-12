bf6f91291c32133d6bbcc0df0ca789135771d0b1
kilokid <arlomov007@gmail.com>
2022-01-12 19:58:14 +0300

    feat: main components connected

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import Header from "../header/Header";
+import Main from "../main/Main";
+
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
