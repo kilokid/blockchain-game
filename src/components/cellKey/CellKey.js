7358f49c54c7db74a466d8f9eb59cec33b679de7
kilokid <arlomov007@gmail.com>
2022-01-14 12:01:32 +0300

    feat: added key and animation of the day of it

--- src/components/cellKey/CellKey.js
+++ src/components/cellKey/CellKey.js
@@ -1,10 +1,14 @@
 import "./cellKey.scss";
 
-import key from '../../resources/img/cell.png';
+import cell from '../../resources/img/cell.png';
+import greenKey from '../../resources/img/greenKey.svg';
 
 const CellKey = () => {
     return (
-        <img className="cell" src={key} alt="cell" />
+        <div className="cellKey">
+            <img className="cellKey__cell" src={cell} alt="cell" />
+            <img className="cellKey__key" src={greenKey} alt="Green key" />
+        </div>
     );
 }
 
